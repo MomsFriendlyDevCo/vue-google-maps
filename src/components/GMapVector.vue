fb0b974e44bda7d5e2ea30d9992bed41e67c1ddb
Mr-Yellow <mr-yellow@mr-yellow.com>
2022-12-13 11:25:39 +1000

    fix: Removed center from options and adjusted watchers

--- src/components/GMapVector.vue
+++ src/components/GMapVector.vue
@@ -205,12 +205,7 @@ export default {
 				}
 			);
 
-			if (this.center && this.center.length === 2) 
-				options.center = {
-					lat: this.center[0],
-					lng: this.center[1]
-				};
-
+			// FIXME: Only apply via watcher? These options may overrule later changes?
 			if (this.maxBounds && this.maxBounds.length === 2 && this.maxBounds[0] && this.maxBounds[0].length === 2 && this.maxBounds[1] && this.maxBounds[1].length === 2)
 				options.restriction = {
 					latLngBounds: {
@@ -270,33 +265,50 @@ export default {
 			// }}}
 
 			// TODO: Encapsulate duplication
-			// FIXME: Can be triggered for reevaluation in cases where nothing has changed?
 			this.$watch('center', () => {
 				this.$debug('center', this.center);
-				if (this.center[0] !== this.mapObject.getCenter().lat() && this.center[1] !== this.mapObject.getCenter().lng())
-					this.mapObject.setCenter({
-						lat: this.center[0],
-						lng: this.center[1],
-					});
-			});
+				if (_.isArray(this.center)) {
+					if (_.isFunction(this.mapObject.getCenter) && _.isObject(this.mapObject.getCenter())) {
+						if (this.center[0] !== this.mapObject.getCenter().lat() && this.center[1] !== this.mapObject.getCenter().lng())
+							this.mapObject.setCenter({
+								lat: this.center[0],
+								lng: this.center[1],
+							});
+					} else {
+						this.mapObject.setCenter({
+							lat: this.center[0],
+							lng: this.center[1],
+						});
+					}
+				} else if (_.isObject(this.center) && _.has(this.center, 'lat') && _.has(this.center, 'lng')) {
+					if (_.isFunction(this.mapObject.getCenter) && _.isObject(this.mapObject.getCenter())) {
+						if (this.center?.lat !== this.mapObject.getCenter().lat() && this.center?.lng !== this.mapObject.getCenter().lng())
+							this.mapObject.setCenter(this.center);
+					} else {
+						this.mapObject.setCenter(this.center);
+					}
+				} else {
+					console.warn('Invalid "center" provided', JSON.stringify(this.center, null, 2));
+				}
+			}, { immediate: true });
 
 			this.$watch('heading', () => {
 				this.$debug('heading', this.heading);
 				if (this.heading !== this.mapObject.getHeading())
 					this.mapObject.setHeading(this.heading);
-			});
+			}, { immediate: true });
 
 			this.$watch('tilt', () => {
 				this.$debug('tilt', this.tilt);
 				if (this.tilt !== this.mapObject.getTilt())
 					this.mapObject.setTilt(this.tilt);
-			});
+			}, { immediate: true });
 
 			this.$watch('zoom', () => {
 				this.$debug('zoom', this.zoom);
 				if (this.zoom !== this.mapObject.getZoom())
 					this.mapObject.setZoom(this.zoom);
-			});
+			}, { immediate: true });
 
 			this.$watch('maxBounds', () => {
 				this.$debug('maxBounds', this.maxBounds);
@@ -312,13 +324,13 @@ export default {
 							strictBounds: true,
 						}
 					});
-			});
+			}, { immediate: true });
 
 			this.$watch('mapTypeId', () => {
 				this.$debug('mapTypeId', this.mapTypeId);
 				if (this.mapTypeId)
 					this.mapObject.setMapTypeId(this.mapTypeId);
-			});
+			}, { immediate: true });
 
 			// TODO: Update zoom and center on pan/zoom, but they're properties... Need to fire an event
 
